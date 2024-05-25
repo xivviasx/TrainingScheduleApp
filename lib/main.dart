04e69a2cab68978c9aa26156e39f5a41aa163173
Agata_Halik <agata10555@gmail.com>
2024-05-25 12:01:27 +0000

    small changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'auth_provider.dart';
+import 'providers/auth_provider.dart';
 import 'loginPage.dart';
 import 'homePage.dart';
 import 'registerPage.dart';
